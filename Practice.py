7436b127417848faef6a8f91d0971675864d8c14
BigMan360 <jafar.myhero@gmail.com>
2023-07-30 21:43:20 +0200

    create csv parser and find max stock method

---
+++ Practice.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+import csv
+from dataclasses import dataclass
+import datetime
+from tokenize import maybe
+from typing import NamedTuple,List
+from dateutil.parser import parse
+@dataclass
+class StockPrice:
+    name: str
+    date: datetime.date
+    openPrice: float
+    high: float
+    low: float
+    close: float
+    adjClose: float
+    volume: int
+    
+    def parseCSV(row:list[str]):
+        try:
+            name,date,openPrice,high,low,close,adjClose,volume=row
+            return StockPrice(name=name,
+                          date=parse(date).date(),
+                          openPrice=float(openPrice),
+                          high=float(high),
+                          low=float(low),
+                          close=float(close),
+                          adjClose=float(adjClose),
+                          volume=float(volume)
+                          )
+        except:
+            print("couldnt parse")
+        
+    
+        
+    
+allStocks: List[StockPrice] = []
+with open("test.csv","r") as csvFile:
+    csvReader = csv.reader(csvFile)
+    header = next(csvReader)
+    for row in csvReader:
+        # 'row' is a list of elements (fields) for each row
+        maybeValid = StockPrice.parseCSV(row)
+        if maybeValid is None:
+            print(f"skipping row: {row}")
+        else: allStocks.append(maybeValid)
+        
+        
+print("Max appl volume is", max([stock.volume for stock in allStocks if stock.name == "AAPL"]))
