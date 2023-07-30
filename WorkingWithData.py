5c862ba7d7ef2923e861255f024094bd44b5cf0d
BigMan360 <jafar.myhero@gmail.com>
2023-07-30 17:48:44 +0200

    create function return the scale of the data

--- WorkingWithData.py
+++ WorkingWithData.py
@@ -1,9 +1,13 @@
+import datetime
 import math,random
 from turtle import width
 from typing import List,Dict
-from collections import Counter
+from collections import Counter, namedtuple
 import matplotlib.pyplot as plt
-import probabilty, vectors,statistics,linecache
+import probabilty, vectors,statistics
+from dateutil.parser import parse
+import csv,re
+
 #this function takes a point and classifies it in a bucket in order to make a histogram of it at a alter point
 def bucketize(point: float, bucketSize:float) -> float:
     #divide by the size and floor it in order to classify data in buckets for the hiustorgram
@@ -28,7 +32,10 @@ uniform = [200*random.random() for i in range(5000)]
 def randomNormalSample()->float:
     return probabilty.inverse_normal_cdf(random.random())
 
-normalSample = [randomNormalSample() for i in range(500000)]
+normalSample = [randomNormalSample() for i in range(5000)]
+normalSample2 = [randomNormalSample() for i in range(5000)]
+matrix=[normalSample,
+        normalSample2]
 #plotHistogram(points=normalSample,bucketSize=0.1,title="normal Test")
 
 #to look at multi dimensional data we can try a correlation matrix, whos i,jth entry is the correlation between these 2 datapoints
@@ -38,4 +45,38 @@ def correleationMatrix(data:List[List]):
         return statistics.correlation(data[i],data[j])
     return vectors.make_matrix(len(data),len(data),iAndjCorrelation)
 
+#print(correleationMatrix(matrix))
+
+heights = [168, 175, 160, 182, 155, 190, 165, 178, 172, 159, 185, 173, 161, 188, 166, 177, 158, 170, 181, 176]
+weights = [65, 70, 58, 80, 52, 90, 62, 75, 68, 56, 78, 70, 60, 85, 63, 72, 55, 67, 79, 74]
+
+#returns stdev and mean for each positon
+def scale(data: List[List[float]]):
+    n= len(data[0])
+    means = vectors.vectorMeans(data)
+    stdevs = [statistics.stdev(vector[i]) for vector in data
+                  for i in range(n)]
+    return means,stdevs
     
+data = [
+    [168, 65],
+    [175, 70],
+    [160, 58],
+    [182, 80],
+    [155, 52],
+    [190, 90],
+    [165, 62],
+    [178, 75],
+    [172, 68],
+    [159, 56],
+    [185, 78],
+    [173, 70],
+    [161, 60],
+    [188, 85],
+    [166, 63],
+    [177, 72],
+    [158, 55],
+    [170, 67],
+    [181, 79],
+    [176, 74]
+]
