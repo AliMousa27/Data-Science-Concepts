a61ade2fc53584af052e13286fec33a457ac3ba9
BigMan360 <jafar.myhero@gmail.com>
2023-07-31 19:33:32 +0200

    add accuarcy, precision and recall functions

--- MachineLearningIntro.py
+++ MachineLearningIntro.py
@@ -6,14 +6,14 @@ Y=TypeVar("Y")#generic data type to represent output
 
 def splitData(data : List[X], percent : float)->Tuple[List[X],List[X]]:
     """Split data into fractions [prob, 1 - prob]"""
-    dataCopy = data[:]
-    random.shuffle(dataCopy)
-    cutIndex = int(len(dataCopy)*percent)
+    data = data[:]
+    random.shuffle(data)
+    cutIndex = int(len(data)*percent)
     #return everything from start to cut and then everything after that as a tuple
-    return dataCopy[:cutIndex],dataCopy[cutIndex:]
+    return data[:cutIndex],data[cutIndex:]
 
 data = [n for n in range(10)]
-print(splitData(data,0.2))
+print(splitData(data=data,percent=0.2))
 
 def train_test_split(xs: List[X],
                      ys: List[Y],
@@ -28,7 +28,8 @@ def train_test_split(xs: List[X],
 xs = [x for x in range(1000)]  # xs are 1 ... 1000
 ys = [2 * x for x in xs]       # each y_i is twice x_i
 x_train, x_test, y_train, y_test = train_test_split(xs, ys, 0.25)
-
 # Check that the proportions are correct
 assert len(x_train) == len(y_train) == 750
 assert len(x_test) == len(y_test) == 250
+assert all(y == 2 * x for x, y in zip(x_train, y_train))
+assert all(y == 2 * x for x, y in zip(x_test, y_test))
