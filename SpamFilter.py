e9bb6b08106d6e3244ee64183d655907a93788d8
BigMan360 <jafar.myhero@gmail.com>
2023-08-05 00:32:42 +0200

    fix bug

--- SpamFilter.py
+++ SpamFilter.py
@@ -1,42 +1,78 @@
-#function to extract all the words and remove duplicates
+from typing import Set
 import re
-import token
-from typing import NamedTuple, List, Tuple,Iterable,Dict,Set
-from collections import defaultdict
-#class for the training data
+
+def tokenize(text: str) -> Set[str]:
+    text = text.lower()                         # Convert to lowercase,
+    all_words = re.findall("[a-z0-9']+", text)  # extract the words, and
+    return set(all_words)                       # remove duplicates.
+
+assert tokenize("Data Science is science") == {"data", "science", "is"}
+
+from typing import NamedTuple
+
 class Message(NamedTuple):
-    text:str
-    isSpam:bool
+    text: str
+    is_spam: bool
 
-def tokenize(text: str)-> set[str]:
-    text=text.lower()
-    allWords = text.split(" ")
-    return set(allWords)
+from typing import List, Tuple, Dict, Iterable
+import math
+from collections import defaultdict
 
-#creating a class sthat keeps track of the counts and labels of the training data
 class NaiveBayesClassifier:
-    #k is the psuedo count to not have unrealisitc probabilities in the data set
-    def __init__(self,k:float = 0.5) -> None:
-        self.tokens=Set[str] = set()
-        #dict where key is word and val is how many times the word appeared in a spam email
+    def __init__(self, k: float = 0.5) -> None:
+        self.k = k  # smoothing factor
+
+        self.tokens: Set[str] = set()
         self.token_spam_counts: Dict[str, int] = defaultdict(int)
-        #ham is not spam email
         self.token_ham_counts: Dict[str, int] = defaultdict(int)
-        #initalize spam and hams to be 0 initally
         self.spam_messages = self.ham_messages = 0
 
-    #the model gets an iterable of messages. 
-    #1. increment spam or ham message based on the bool
-    #2. add them to the dict based on if they are spam or ham
-    def train(self,messages: Iterable[Message]) -> None:
+    def train(self, messages: Iterable[Message]) -> None:
         for message in messages:
-            if message.isSpam:
-                self.spam_messages+=1
+            # Increment message counts
+            if message.is_spam:
+                self.spam_messages += 1
             else:
-                self.ham_messages+=1
-                #then for each of the words we increment the dict val counts based on if its spam or ham
-            for token in tokenize(message):
-                if message.isSpam:
-                    self.token_spam_counts[token]+=1
+                self.ham_messages += 1
+
+            # Increment word counts
+            for token in tokenize(message.text):
+                self.tokens.add(token)
+                if message.is_spam:
+                    self.token_spam_counts[token] += 1
                 else:
-                    self.token_ham_counts[token]+=1
+                    self.token_ham_counts[token] += 1
+
+    def _probabilities(self, token: str) -> Tuple[float, float]:
+        """returns P(token | spam) and P(token | not spam)"""
+        spam = self.token_spam_counts[token]
+        ham = self.token_ham_counts[token]
+
+        p_token_spam = (spam + self.k) / (self.spam_messages + 2 * self.k)
+        p_token_ham = (ham + self.k) / (self.ham_messages + 2 * self.k)
+
+        return p_token_spam, p_token_ham
+
+    def predict(self, text: str) -> float:
+        text_tokens = tokenize(text)
+        log_prob_if_spam = log_prob_if_ham = 0.0
+
+        # Iterate through each word in our vocabulary.
+        for token in self.tokens:
+            prob_if_spam, prob_if_ham = self._probabilities(token)
+
+            # If *token* appears in the message,
+            # add the log probability of seeing it;
+            if token in text_tokens:
+                log_prob_if_spam += math.log(prob_if_spam)
+                log_prob_if_ham += math.log(prob_if_ham)
+
+            # otherwise add the log probability of _not_ seeing it
+            # which is log(1 - probability of seeing it)
+            else:
+                log_prob_if_spam += math.log(1.0 - prob_if_spam)
+                log_prob_if_ham += math.log(1.0 - prob_if_ham)
+
+        prob_if_spam = math.exp(log_prob_if_spam)
+        prob_if_ham = math.exp(log_prob_if_ham)
+        return prob_if_spam / (prob_if_spam + prob_if_ham)
